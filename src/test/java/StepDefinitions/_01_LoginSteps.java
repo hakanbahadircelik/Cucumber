25b5ebc28e49848a8d72222c8e00aaf977f691c3
Baha <hakanbahadircelik@gmail.com>
2025-03-17 16:10:44 +0400

    sout test

--- src/test/java/StepDefinitions/_01_LoginSteps.java
+++ src/test/java/StepDefinitions/_01_LoginSteps.java
@@ -6,15 +6,17 @@ public class _01_LoginSteps {
 
     @Given("Navigate to Campus")
     public void navigate_to_campus() {
-
+        System.out.println("website opened");
     }
+
     @When("Enter username and password click login button")
     public void enter_username_and_password_click_login_button() {
-
+        System.out.println("username and password written");
     }
+
     @Then("User should login successfully")
     public void user_should_login_successfully() {
-
+        System.out.println("user logged in successfully");
     }
 
 }
