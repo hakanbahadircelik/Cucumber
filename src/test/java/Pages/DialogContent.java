51861068b677ebf40dcb1158ac0180fa0c6d9175
Baha <hakanbahadircelik@gmail.com>
2025-03-27 16:08:12 +0400

    make the parent class because of, if you use 1 thing in every situation, we can create a class name with parent and extend on other classes.

--- src/test/java/Pages/DialogContent.java
+++ src/test/java/Pages/DialogContent.java
@@ -11,7 +11,7 @@ import org.testng.Assert;
 
 import java.time.Duration;
 
-public class DialogContent {
+public class DialogContent extends Parent{
 
     public DialogContent() {
         PageFactory.initElements(GWD.getDriver(), this);
@@ -29,36 +29,5 @@ public class DialogContent {
 
     @FindBy(xpath = "//span[text()='Internship']")
     public WebElement txtInternship;
-
-    public void myClick(WebElement element) {
-
-        WebDriverWait wait = new WebDriverWait(GWD.getDriver(), Duration.ofSeconds(20));
-        wait.until(ExpectedConditions.elementToBeClickable(element));
-
-        scrollToElement(element);
-        element.click();
-    }
-
-    public void mySendKeys(WebElement element, String written) {
-
-        WebDriverWait wait = new WebDriverWait(GWD.getDriver(), Duration.ofSeconds(20));
-        wait.until(ExpectedConditions.visibilityOf(element));
-
-        scrollToElement(element);
-        element.clear();
-        element.sendKeys(written);
-    }
-
-    public void scrollToElement(WebElement element) {
-        JavascriptExecutor js = (JavascriptExecutor) GWD.getDriver();
-        js.executeScript("arguments[0].scrollIntoView();", element);
-    }
-
-    public void verifyText(WebElement element, String txt) {
-        WebDriverWait wait = new WebDriverWait(GWD.getDriver(), Duration.ofSeconds(20));
-        wait.until(ExpectedConditions.visibilityOf(element));
-
-        Assert.assertTrue(element.getText().toLowerCase().contains(txt));
-    }
 
 }
