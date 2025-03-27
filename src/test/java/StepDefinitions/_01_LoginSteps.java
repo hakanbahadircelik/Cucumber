e5fe5da2a6293b6d995964aae2deeaade2cd0c40
Baha <hakanbahadircelik@gmail.com>
2025-03-27 15:36:30 +0400

    easy way to open loginSteps

--- src/test/java/StepDefinitions/_01_LoginSteps.java
+++ src/test/java/StepDefinitions/_01_LoginSteps.java
@@ -10,31 +10,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
 
 public class _01_LoginSteps {
-    DialogContent dc = new DialogContent();
 
-    @Given("Navigate to Campus")
-    public void navigate_to_campus() {
-        GWD.getDriver().get("https://test.mersys.io/");
-    }
 
-    @When("Enter username and password click login button")
-    public void enter_username_and_password_click_login_button() {
-
-//        WebDriverWait wait = new WebDriverWait(GWD.getDriver(), Duration.ofSeconds(20));
-//        wait.until(ExpectedConditions.visibilityOf(dc.username));
-//
-//        dc.username.sendKeys("turkeyts");
-//        dc.password.sendKeys("Techno2025.%");
-//        dc.loginBtn.click();
-
-        dc.click(dc.loginBtn);
-        dc.sendKeys(dc.username,"turkeyts");
-        dc.sendKeys(dc.password,"Techno2025.%");
-    }
-
-    @Then("User should login successfully")
-    public void user_should_login_successfully() {
-        System.out.println("web site is opened tested");
-    }
 
 }
