ce345c1a283e8906d8f1d8cccd0720a30060128b
Baha <hakanbahadircelik@gmail.com>
2025-03-28 03:53:12 +0400

    multiple parameters using in ones

--- src/test/java/FeatureFiles/_07_DataTableExample.feature
+++ src/test/java/FeatureFiles/_07_DataTableExample.feature
@@ -3,3 +3,14 @@ Feature: DataTable Example
   Scenario: Users List
     When write username "baha"
     And Write username and Password "baha" and "1234"
+    
+    And Write username as DataTable
+    | baha |
+    | jack |
+    | sean |
+    | jon  |
+    
+    And Write username and Password as DataTable
+    | baha | 1234   |
+    | jack | 12345  |
+    | sean | 123456 |
--- src/test/java/StepDefinitions/_07_DataTableExample.java
+++ src/test/java/StepDefinitions/_07_DataTableExample.java
@@ -1,20 +1,32 @@
 package StepDefinitions;
 
+import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.When;
+
+import java.util.List;
 
 public class _07_DataTableExample {
 
     @When("write username {string}")
-    public void writeUsername(String arg0) {
-
-
+    public void writeUsername(String userName) {
+        System.out.println("userName = " + userName);
     }
 
     @And("Write username and Password {string} and {string}")
-    public void writeUsernameAndPasswordAnd(String arg0, String arg1) {
-
-
+    public void writeUsernameAndPasswordAnd(String userName, String password) {
+        System.out.println("userName = " + userName);
+        System.out.println("password = " + password);
     }
+
+    @And("Write username as DataTable")
+    public void writeUsernameAsDataTable(DataTable users) {
+        List<String> listUsers = users.asList(String.class);
+
+        for (String user : listUsers)
+            System.out.println("user = " + user);
+    }
+
+
 
 }
