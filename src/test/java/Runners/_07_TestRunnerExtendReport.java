4c1efc67a2b7636469aabe470c60b061f289940f
Baha <hakanbahadircelik@gmail.com>
2025-04-15 09:03:35 +0400

    extend report build-up

---
+++ src/test/java/Runners/_07_TestRunnerExtendReport.java
@@ -0,0 +1,13 @@
+package Runners;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        tags = "@SmokeTest",
+        features = {"src/test/java/FeatureFiles"},
+        glue = {"StepDefinitions"},
+        plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"} // simple report
+)
+public class _07_TestRunnerExtendReport extends AbstractTestNGCucumberTests {
+}
