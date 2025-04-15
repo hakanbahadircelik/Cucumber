1fc9d3e7e864191574ab455f1ca6b04cffb5d24b
Baha <hakanbahadircelik@gmail.com>
2025-04-15 10:33:49 +0400

    parallel test on cucumber integration

---
+++ src/test/java/Runners/_08_TestRunnerParalelTest.java
@@ -0,0 +1,37 @@
+package Runners;
+
+import Utilities.GWD;
+import com.aventstack.extentreports.service.ExtentService;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+@CucumberOptions(
+        tags = "@SmokeTest",
+        features = {"src/test/java/FeatureFiles"},
+        glue = {"StepDefinitions"},
+        plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"} // simple report
+)
+public class _08_TestRunnerParalelTest extends AbstractTestNGCucumberTests {
+
+    @BeforeClass
+    public void beforeClass(String browserName) {
+        GWD.threadBrowserName.set(browserName);
+    }
+
+    @AfterClass
+    public static void writeExtentReport() {
+        ExtentService.getInstance().setSystemInfo("Windows User Name", System.getProperty("user.name"));
+        ExtentService.getInstance().setSystemInfo("Time Zone", System.getProperty("user.timezone"));
+        ExtentService.getInstance().setSystemInfo("User Name", "İsmet Temur");
+        ExtentService.getInstance().setSystemInfo("Application Name", "Campus");
+        ExtentService.getInstance().setSystemInfo("Operating System Info", System.getProperty("os.name"));
+        ExtentService.getInstance().setSystemInfo("Department", "QA");
+        ExtentService.getInstance().setSystemInfo("Ek Satır", "Açıklama");
+        ExtentService.getInstance().setSystemInfo("Ek Satır", "Açıklama");
+        ExtentService.getInstance().setSystemInfo("Ek Satır", "Açıklama");
+        ExtentService.getInstance().setSystemInfo("Ek Satır", "Açıklama");
+    }
+
+}
--- src/test/java/Utilities/GWD.java
+++ src/test/java/Utilities/GWD.java
@@ -2,6 +2,7 @@ package Utilities;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
@@ -11,6 +12,7 @@ import java.util.Locale;
 public class GWD {
 
     private static ThreadLocal<WebDriver> threadDriver = new ThreadLocal<>();
+    public static ThreadLocal<String> threadBrowserName = new ThreadLocal<>();
 
     // threadDriver.get() -> take driver from the thread(boru)
     // threadDriver.set(driver) -> set a driver to the thread(boru)
@@ -20,14 +22,19 @@ public class GWD {
         Locale.setDefault(new Locale("EN"));
         System.setProperty("user.language", "EN");
 
+        if(threadBrowserName.get()==null)
+            threadBrowserName.set("chrome");
+
         if (threadDriver.get() == null) {  //  work 1 time at First
 
-            //  with name of the browser, create a switch and set it.
-            threadDriver.set(new ChromeDriver());
-            threadDriver.get().manage().window().maximize();
-            threadDriver.get().manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20)); // loading page wait till 30sec, or ERROR
+            switch (threadBrowserName.get()){
+                case "edge" : threadDriver.set(new EdgeDriver()); break;
+                case "chrome" : threadDriver.set(new ChromeDriver()); break;
+            }
         }
-
+//  with the name of the browser, create a switch and set it.
+        threadDriver.get().manage().window().maximize();
+        threadDriver.get().manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20)); // loading page wait till 30sec, or ERROR
         return threadDriver.get();
     }
 
