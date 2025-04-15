ef7b0401b9d749f4fc66a84fcf37a54bffb04a3a
Baha <hakanbahadircelik@gmail.com>
2025-04-15 09:37:39 +0400

    detail info for test

--- src/test/java/Runners/_07_TestRunnerExtendReport.java
+++ src/test/java/Runners/_07_TestRunnerExtendReport.java
@@ -1,7 +1,9 @@
 package Runners;
 
+import com.aventstack.extentreports.service.ExtentService;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.AfterClass;
 
 @CucumberOptions(
         tags = "@SmokeTest",
@@ -10,4 +12,11 @@ import io.cucumber.testng.CucumberOptions;
         plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"} // simple report
 )
 public class _07_TestRunnerExtendReport extends AbstractTestNGCucumberTests {
+
+    @AfterClass
+    public static void writeExtentReport(){
+        ExtentService.getInstance().setSystemInfo("",""); // username : baha
+        // adding extra info to the report of test
+    }
+
 }
