6c5e3f38b3fdb0a2537df4f0123f7b7e924385fd
Baha <hakanbahadircelik@gmail.com>
2025-05-04 06:19:15 +0400

    different type of examples

--- src/test/java/_JDBC/Day2/_01_GetAllRowColumn.java
+++ src/test/java/_JDBC/Day2/_01_GetAllRowColumn.java
@@ -6,6 +6,8 @@ import org.testng.annotations.Test;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class _01_GetAllRowColumn extends JDBCParent {
 
@@ -44,6 +46,40 @@ public class _01_GetAllRowColumn extends JDBCParent {
             System.out.println(resultSetMetaData.getColumnName(i) + "\t" + resultSetMetaData.getColumnTypeName(i) + " ");
         }
 
+    }
+
+    @Test
+    public void test03() {
+
+        String statementt = "select * from language";
+        List<List<String>> returnedList = getListData(statementt);
+        System.out.println("returnedList = " + returnedList);
+
+    }
+
+    public List<List<String>> getListData(String statementt) {
+        List<List<String>> table = new ArrayList<>();
+
+        try {
+            ResultSet resultSet = statement.executeQuery(statementt);
+            ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
+
+            while (resultSet.next()){
+                List<String> column = new ArrayList<>();
+                for (int i = 1; i < resultSetMetaData.getColumnCount(); i++) {
+                    column.add(resultSet.getString(i));
+                }
+
+                table.add(column);
+            }
+
+        }
+        catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+
+
+        return table;
     }
 
 }
